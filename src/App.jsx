6733adf932f33001d67991b67e4f643832556579
JakubCzajor <jakub.czajor7@gmail.com>
2023-09-15 20:55:40 +0200

    Added some basic mechanics

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Card from "./components/Card"
 
 export default function App() {
 
+    const [game, setGame] = useState(false)
     const [deck, setDeck] = useState({})
     const [playerCards, setPlayerCards] = useState([{}])
     const [playerCardValue, setPlayerCardValue] = useState(0)
@@ -11,30 +12,53 @@ export default function App() {
         fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
         .then(res => res.json())
         .then(data => setDeck(data))
-    }, [])
+    }, [game])
 
+    function setupGame() {
+        setGame(true)
+        drawCard(2)
+    }
 
-    const drawCard = async() => {
+    // useEffect(() => {
+    //     checkGameOver()
+    // }, [playerCardValue])
+
+    // function checkGameOver() {
+    //     if(playerCardValue > 21) {
+    //         setIsGameOver(true)
+    //     }
+    //     playerCardValue > 21 ? console.log("You lost") : console.log("Game still")
+        
+    // }
+
+    const drawCard = async(num) => {
         try {
-            const data = await (await fetch(`https://www.deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`)).json()
-            setPlayerCards(oldDeck => {
-                return [...oldDeck, data.cards[0]]
+            const data = await (
+                await fetch(`https://www.deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=${num}`)
+            ).json()
+            const newCards = data.cards
+            newCards.forEach(element => {
+                setPlayerCards(oldDeck => {
+                    return [...oldDeck, element]
+                })
+                updateCardValue(element.value)
             })
-            setPlayerCardValue(oldValue => oldValue + parseInt(data.cards[0].value))
         } catch (err) {
             console.log(err.message)
         }
     }
 
     function updateCardValue(card) {
-        
+        const value = isNaN(card) ? 10 : parseInt(card)
+        setPlayerCardValue(oldValue => oldValue + value)
     }
 
     const cardElements = playerCards.map(card => <Card image={card.image} value={card.value} />)
 
     return (
     <main>
-        <button onClick={drawCard}>Hit</button>
+        {game === false ? <button onClick={setupGame}>Start Game</button> : ""}
+        <button onClick={() => drawCard(1)}>Hit</button> <br />
         {playerCardValue}
         <div className="player--container">
             {cardElements}
